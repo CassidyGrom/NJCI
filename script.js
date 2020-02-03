59197dc28ba89a521f4d787e42978e705afaa7d9
Cassidy Grom <cgrom@njadvancemedia.com>
2020-02-03 17:36:23 -0500

    appearFunction working

--- script.js
+++ script.js
@@ -1,17 +1,33 @@
 //Get the button
 var mybutton = document.getElementById("myBtn");
+var mobileBarEl = document.getElementById("subscriptionBar");
+mobileBarEl.style.color = "purple";
 
-// When the user scrolls down 20px from the top of the document, show the button
+var x = window.matchMedia("(max-width:1200px)");
+
 window.onscroll = function() {
   scrollFunction();
 };
+
 function scrollFunction() {
   if (
-    document.body.scrollTop > 500 ||
     document.documentElement.scrollTop > 500
   ) {
     mybutton.style.display = "block";
+    // mobileBarEl.style.display = "block";
   } else {
     mybutton.style.display = "none";
+    // mobileBarEl.style.display = "none";
   }
 }
+
+function appearFunction(x) {
+  if (x.matches) {
+    mobileBarEl.style.display = "block";
+  } else {
+    mobileBarEl.style.display = "none";
+  }
+}
+
+appearFunction(x);
+x.addListener(appearFunction);
